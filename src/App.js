912e1553047b801bfdec720bada10e4cba442982
askari-fahlyani <askari.fahlyani@gmail.com>
2024-01-15 11:14:55 +0100

    tailwind css added and configed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import './style/style.css'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className='initial-style'>
+    <div className='h-[500px] grid grid-cols-[repeat(auto-fit,_minmax(150px,_auto))]'>
+  <div className='bg-[green]'>
+
+  </div>
+  <div className='bg-[red]'>
+
+  </div>
+  <div className='bg-[blue]'>
+
+  </div>
     </div>
+  </div>
   );
 }
 
